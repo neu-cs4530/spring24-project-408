136899116a0e935b50692de23dcde0623b62781a
TNKR5 <156848288+TNKR5@users.noreply.github.com>
2024-03-12 21:39:23 -0400

    Added comments

--- final-project-classes/GameObject.ts
+++ final-project-classes/GameObject.ts
@@ -1,7 +1,21 @@
+/**
+* BLOCK_WIDTH is the size of a 'Block' in the game.
+* Space in the game is broken up into blocks.
+*/
 const BLOCK_WIDTH = 255; // This value can be changed depending on what's easier for future use
 
+/**
+* A Gameunit is a type that is a number in the game.
+* It represents a unit of the game.
+*/
 export type GameUnit = number;
 
+/**
+* A GameObject is an object in the Mario game. 
+* @param _x represents the x coordinate value of a GameObject.
+* @param _y represents the y coordinate value of a GameObject. 
+* @param _gameLetter is a letter used to represent a GameObject in the game.
+*/
 export abstract class GameObject {
     _x: GameUnit;
     _y: GameUnit;
@@ -12,15 +26,22 @@ export abstract class GameObject {
         this._y = newY;
         this._gameLetter = newGameLetter;
     }
-
+    /**
+    * Converts the _gameLetter to a string.
+    */
     public toString() {
         return this._gameLetter;
     }
 
+  /**
+  * Gets the x coordinate value of a GameObject.
+  */
     public get x() {
         return this._x;
     }
-
+/**
+* Gets the y coordinate value of a GameObject.
+*/
     public get y() {
         return this._y;
     }
