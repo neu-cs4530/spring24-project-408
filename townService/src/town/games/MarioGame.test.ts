cef6c1a678b1d53050de75376e17378a3edbe2b4
devanshis12 <shah.devan@northeastern.edu>
2024-03-27 21:07:08 -0400

    added all tests for MarioGame.ts

--- townService/src/town/games/MarioGame.test.ts
+++ townService/src/town/games/MarioGame.test.ts
@@ -1,13 +1,7 @@
-import { Console } from 'console';
-import e from 'cors';
-import { o } from 'ramda';
 import MarioGame from './MarioGame';
 import {
-  BOARD_POSITION_NOT_VALID_MESSAGE,
   GAME_FULL_MESSAGE,
   GAME_NOT_IN_PROGRESS_MESSAGE,
-  GAME_NOT_STARTABLE_MESSAGE,
-  MOVE_NOT_YOUR_TURN_MESSAGE,
   PLAYER_ALREADY_IN_GAME_MESSAGE,
   PLAYER_NOT_IN_GAME_MESSAGE,
 } from '../../lib/InvalidParametersError';
@@ -34,6 +28,7 @@ describe('MarioGame', () => {
     });
     it('should set the status to IN_PROGRESS if player is present', () => {
       const player = createPlayerForTesting();
+      expect(game.state.status).toBe('WAITING_FOR_PLAYERS');
       game.join(player);
       expect(game.state.status).toBe('IN_PROGRESS');
       expect(game.state.player).toBe(player.id);
@@ -54,24 +49,21 @@ describe('MarioGame', () => {
       game.leave(player);
       expect(game.state.status).toBe('OVER');
       expect(game.state.player).toBeUndefined();
-      expect(game.state.health).toBe(3);
       expect(game.state.winner).toBeUndefined();
       expect(game.state.score).toBe(0);
     });
   });
   describe('applyMove', () => {
     let rightMove: MarioMove;
-    let leftMove: MarioMove;
     let upMove: MarioMove;
+    const player1 = createPlayerForTesting();
     beforeEach(() => {
-      const player1 = createPlayerForTesting();
       game.join(player1);
       rightMove = { gamePiece: 'Mario', row: 0, col: 1 };
-      leftMove = { gamePiece: 'Mario', row: 0, col: -1 };
       upMove = { gamePiece: 'Mario', row: 1, col: 0 };
     });
     it('should throw an error if the game is not in progress', () => {
-      game.leave(createPlayerForTesting());
+      game.leave(player1);
       expect(() =>
         game.applyMove({ gameID: game.id, playerID: createPlayerForTesting().id, move: rightMove }),
       ).toThrowError(GAME_NOT_IN_PROGRESS_MESSAGE);
@@ -84,13 +76,13 @@ describe('MarioGame', () => {
     it('should add the move to the game state', () => {
       if (game.state.player) {
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
-        expect(game.state.moves).toEqual(['right']);
+        expect(game.state.moves).toEqual([rightMove]);
       }
     });
     it('should update the score', () => {
       if (game.state.player) {
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
-        expect(game.state.score).toBe(1);
+        expect(game.state.score).toBe(100);
       }
     });
     it('should change game state to OVER if the player wins and set winner - here we are doing a FULL runthrough of level one', () => {
@@ -117,7 +109,7 @@ describe('MarioGame', () => {
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: upMove });
         game._level.onTick();
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
-        //GOOMBA SHOULD BE DEAD HERE
+        // GOOMBA SHOULD BE DEAD HERE
         game._level.onTick();
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
@@ -201,6 +193,7 @@ describe('MarioGame', () => {
     });
     it('should change game state to OVER if the player loses health 3 times', () => {
       if (game.state.player) {
+        console.log('FIRST HEART');
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
@@ -219,58 +212,64 @@ describe('MarioGame', () => {
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
+        game._level.onTick();
+        console.log(game._level._mario._x, game._level._mario._y);
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: upMove });
+        game._level.onTick();
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
-        game._level.onTick();
-        console.log(game._level.toString());
-        game._level.onTick();
-        console.log(game._level.toString());
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         expect(game.state.status).toBe('IN_PROGRESS');
         expect(game._level._mario._health).toBe(2);
         expect(game._level._mario._x).toBe(game._level._startingMarioPos[0]);
         expect(game._level._mario._y).toBe(game._level._startingMarioPos[1]);
         game._level.onTick();
-        console.log("DEATH HERE");
+        console.log('DEATH HERE');
 
-        console.log("2ND HEART STARTS HERE");
+        console.log('SECOND HEART');
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: upMove });
         game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: upMove });
         game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
+        game._level.onTick();
+        console.log(game._level._mario._x, game._level._mario._y);
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: upMove });
+        game._level.onTick();
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
-        console.log(game._level.toString());
-        game._level.onTick();
-        console.log(game._level.toString());
-        game._level.onTick();
-        console.log(game._level.toString());
+        console.log(game._level._mario._x, game._level._mario._y);
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         expect(game.state.status).toBe('IN_PROGRESS');
         expect(game._level._mario._health).toBe(1);
         expect(game._level._mario._x).toBe(game._level._startingMarioPos[0]);
         expect(game._level._mario._y).toBe(game._level._startingMarioPos[1]);
         game._level.onTick();
-        console.log("DEATH HERE");
+        console.log('DEATH HERE');
 
-        console.log("LAST HEART HERE");
+        console.log('LAST HEART');
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
@@ -289,22 +288,20 @@ describe('MarioGame', () => {
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
+        game._level.onTick();
+        console.log(game._level._mario._x, game._level._mario._y);
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: upMove });
+        game._level.onTick();
+        console.log(game._level._mario._x, game._level._mario._y);
         game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         game._level.onTick();
         console.log(game._level._mario._x, game._level._mario._y);
-        game._level.onTick();
-        console.log(game._level.toString());
-        game._level.onTick();
-        console.log(game._level.toString());
-        game._level.onTick();
-        console.log(game._level.toString());
-        
+        game.applyMove({ gameID: game.id, playerID: game.state.player, move: rightMove });
         expect(game._level._mario._health).toBe(0);
-        console.log(game._level._gameState);
         expect(game.state.status).toBe('OVER');
-        expect(game.state.winner).toBe(undefined);
       }
     });
   });
